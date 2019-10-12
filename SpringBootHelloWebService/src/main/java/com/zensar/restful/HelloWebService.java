24b3b58730451f251e536a81db969253114cdb15
Garvita Jain <jaingarvita82@gmail.com>
2019-10-12 12:21:11 -0700

    Added Spring boot first project

---
+++ SpringBootHelloWebService/src/main/java/com/zensar/restful/HelloWebService.java
@@ -0,0 +1,13 @@
+package com.zensar.restful;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWebService {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Welcome to Zensar Web Services using Spring Boot";
+
+	}
+}
